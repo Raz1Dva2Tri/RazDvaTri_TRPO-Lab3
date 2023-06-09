fcb559de2dc3b415400045fd5bbfc169d38d68d6
Raz1Dva2Tri <126345749+Raz1Dva2Tri@users.noreply.github.com>
2023-06-10 02:38:04 +0500

    Добавил функционал класса

---
+++ RazDvaTri_TRPO-Lab3/AreaTriangleGerone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazDvaTri_TRPO_Lab3
+{
+    internal class Triangle
+    {
+        double a;
+        double b;
+        double c;
+
+        public Triangle(double a_, double b_, double c_)
+        {
+            a = a_;
+            b = b_;
+            c = c_;
+        }
+
+        double p
+        {
+            get { return a+b+c; }
+        }
+        double S
+        {
+            get { return Math.Sqrt(p*(p-a)* (p - b) * (p - c)); }
+        }
+    }
+}
